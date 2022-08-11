e44f2836adfebd5aa96c65a827f9ccd65dabf3c2
Devyoma <lawrenceyoma@gmail.com>
2022-08-11 15:11:15 +0100

    fix: added netlify redirects

--- src/Firebase/Firebase.ts
+++ src/Firebase/Firebase.ts
@@ -6,14 +6,15 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfxB_O2mK1GBnu0wPXgaVTdRUh9vywkLE",
-  authDomain: "nfcs-1e729.firebaseapp.com",
-  projectId: "nfcs-1e729",
-  storageBucket: "nfcs-1e729.appspot.com",
-  messagingSenderId: "93473949829",
-  appId: "1:93473949829:web:0d4db562e7d77c2fbec1ec",
-  measurementId: "G-8FZDBTS0EJ"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
