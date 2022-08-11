4beac9d173d3b3dc8773c55440006cc6a1903c57
Devyoma <lawrenceyoma@gmail.com>
2022-08-11 15:38:22 +0100

    fix: 2nd fix for invalid api-key error

--- src/Firebase/Firebase.ts
+++ src/Firebase/Firebase.ts
@@ -6,7 +6,8 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  // apiKey: `${process.env.REACT_APP_API_KEY}`,
+  apiKey: "process.env.REACT_APP_API_KEY",
   authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
   projectId: `${process.env.REACT_APP_PROJECT_ID}`,
   storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
